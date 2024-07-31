bc0cfc7c2ddaa86195fcbf008330f57f75dd1110
Tarshid Akhtar <tarshidakhtar@Tarshids-MacBook-Pro.local>
2024-07-31 20:04:49 +0530

    bug fixes

--- src/componenets/NavBar.jsx
+++ src/componenets/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
     <div className='flex border space-x-20 items-centre pl-3 py-4'>
-    <h3 className='text-blue-400'>Movies</h3>
+    <h3 className='text-blue-500'>Movies</h3>
     <h3 className='text-blue-400'>WatchList</h3>
     </div>
   )
