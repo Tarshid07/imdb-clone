a6cde9037243145f8ba4e3f15c046f63175cb5f3
Tarshid Akhtar <tarshidakhtar@Tarshids-MacBook-Pro.local>
2024-08-03 13:21:16 +0530

    banner bug fixes

--- src/componenets/Banner.jsx
+++ src/componenets/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='h-[18vh] md:h-[60vh] bg-center bg-no-repeat flex items-end'
+    <div className='h-[18vh] md:h-[30vh] bg-center bg-no-repeat flex items-end'
     style={{backgroundImage : `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaVSWCaGAWiUF0P3OPwKFomwBtcc77AGc35w&s)`}}>
         <div className='text-xl md:text-3xl bg-gray-900 bg-opacity-60 text-white text-center w-full'>
             Avengers
